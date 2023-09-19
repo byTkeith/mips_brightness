a315bb8bfe5766f9a9911130c0e8dbb67c8b6977
Tendai <youremail@example.com>
2023-09-19 20:30:41 +0200

    open the file

--- increase_brightness.asm
+++ increase_brightness.asm
@@ -17,3 +17,156 @@
         li $a1, 0              #reads e the file 
         li $a2, 0             
         syscall
+    # Check if the file was opened successfully
+        bnez $v0, file_opened
+
+        # If the file could not be opened, exit
+        li $v0, 10             # Load syscall code for "exit" (10)
+        syscall  
+
+    file_opened:
+        move $s0, $v0          # Save file descriptor to $s0
+
+        # Open the new file for writing
+        li $v0, 13             # Load syscall code for "open" (13)
+        la $a0, output_file  # Load the address of the new file name
+        li $a1, 1              # Flag: write mode
+        li $a2, 0              # File permission (ignored in write mode)
+        syscall
+
+        # Check if the file was opened successfully
+        bnez $v0, new_file_opened
+
+        # If the file could not be opened, close the original file and exit
+        li $v0, 16             # Load syscall code for "close" (16)
+        move $a0, $s0          # Pass file descriptor of the original file
+        syscall
+
+        li $v0, 10             # Load syscall code for "exit" (10)
+        syscall
+
+    new_file_opened:
+        move $s1, $v0          # Save file descriptor to $s1
+
+        # Read the PPM header (3 lines)
+        li $t0, 0              # Initialize a counter
+        la $t1, image_width         # Load the address of the image_width
+
+    read_header_loop:
+        beq $t0, 3, read_pixels # If 3 lines are read, proceed to read pixels
+        li $v0, 14             # Load syscall code for "read" (14)
+        move $a0, $s0          # Original file descriptor
+        move $a1, $t1          # Destination image_width
+        li $a2, 512            # Read up to 512 characters
+        syscall
+        addi $t0, $t0, 1       # Increment the counter
+        addi $t1, $t1, 512     # Move image_width pointer to the next position
+        b read_header_loop
+
+    read_pixels:
+        li $t2, 0              # Initialize a counter for the number of pixels
+
+    read_pixel_loop:
+        beq $t2, 512, write_new_image # If 512 pixels are read, proceed to write the new image
+
+        # Read a pixel (3 RGB values)
+        li $v0, 14             # Load syscall code for "read" (14)
+        move $a0, $s0          # Original file descriptor
+        move $a1, $t1          # Destination image_width
+        li $a2, 3              # Read 3 bytes (RGB values)
+        syscall
+
+        # Increment image_width pointer
+        addi $t1, $t1, 3
+
+        # Calculate new RGB values with a cap at 255
+        lb $t3, -3($t1)        # Load the R component
+        lb $t4, -2($t1)        # Load the G component
+        lb $t5, -1($t1)        # Load the B component
+
+        addi $t3, $t3, 10      # Increase R by 10
+        addi $t4, $t4, 10      # Increase G by 10
+        addi $t5, $t5, 10      # Increase B by 10
+
+        # Check for cap at 255
+        li $t6, 255            # Load 255 into $t6
+        min $t3, $t3, $t6      # Cap R at 255
+        min $t4, $t4, $t6      # Cap G at 255
+        min $t5, $t5, $t6      # Cap B at 255
+
+        # Write the new RGB values
+        sb $t3, -3($t1)        # Write the new R component
+        sb $t4, -2($t1)        # Write the new G component
+        sb $t5, -1($t1)        # Write the new B component
+
+        # Increment the pixel counter
+        addi $t2, $t2, 1
+        b read_pixel_loop
+
+    write_new_image:
+        # Write the modified image to the new file
+        li $v0, 15             # Load syscall code for "write" (15)
+        move $a0, $s1          # New file descriptor
+        la $a1, image_width         # Source image_width
+        li $a2, 512            # Write up to 512 characters
+        syscall
+
+        # Close both files
+        li $v0, 16             # Load syscall code for "close" (16)
+        move $a0, $s0          # Original file descriptor
+        syscall
+        move $a0, $s1          # New file descriptor
+        syscall
+
+        # Calculate and display the average pixel values
+        li $t7, 0              # Initialize a counter for the total RGB values in the original image
+        li $t8, 0              # Initialize a counter for the total RGB values in the new image
+        la $t1, image_width         # Reset the image_width pointer
+
+        calculate_averages:
+            beq $t1, $t2, display_averages  # If the image_width pointer reaches the end, calculate and display averages
+
+            # Calculate the sum of RGB values in the original image
+            lb $t9, -3($t1)        # Load the R component
+            lb $t10, -2($t1)       # Load the G component
+            lb $t11, -1($t1)       # Load the B component
+
+            add $t7, $t7, $t9      # Add R component to the total for original image
+            add $t7, $t7, $t10     # Add G component to the total for original image
+            add $t7, $t7, $t11     # Add B component to the total for original image
+
+            # Calculate the sum of RGB values in the new image
+            lb $t12, -3($t1)       # Load the modified R component
+            lb $t13, -2($t1)       # Load the modified G component
+            lb $t14, -1($t1)       # Load the modified B component
+
+            add $t8, $t8, $t12     # Add modified R component to the total for new image
+            add $t8, $t8, $t13     # Add modified G component to the total for new image
+            add $t8, $t8, $t14     # Add modified B component to the total for new image
+
+            addi $t1, $t1, 3       # Move image_width pointer to the next pixel
+            j calculate_averages
+
+        display_averages:
+            # Calculate the number of pixels
+            div $t2, $t7
+            mflo $f0               # Store the result in $f0
+
+            # Display the average pixel value of the original image
+            li $v0, 2              # Load syscall code for "print float" (2)
+            syscall
+
+            # Calculate the number of pixels
+            div $t2, $t8
+            mflo $f0               # Store the result in $f0
+
+            # Display the average pixel value of the new image
+            li $v0, 2              # Load syscall code for "print float" (2)
+            syscall
+
+        # Exit the program
+        li $v0, 10             # Load syscall code for "exit" (10)
+        syscall
+
+
+         
