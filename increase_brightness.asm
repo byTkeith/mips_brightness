b5b755be0fbccc65764954f1357f5b755707f63d
Tendai <youremail@example.com>
2023-09-19 20:28:19 +0200

    opened the file to be read

--- increase_brightness.asm
+++ increase_brightness.asm
@@ -8,3 +8,12 @@
     input_file:  .asciiz "sample_images (1)/house_64_in_ascii_crlf.ppm"     #input file name
     output_file:   .asciiz "house_64_in_ascii_crlf_output.ppm"    # New output file name
     image_width:         .space 1024             # image_width for reading and writing 
+
+.text
+    main:
+        # Open the original file for reading
+        li $v0, 13             #syscall 13 opens the file
+        la $a0, input_file #loadsthe address of the file
+        li $a1, 0              #reads e the file 
+        li $a2, 0             
+        syscall
