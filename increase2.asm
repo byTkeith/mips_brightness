66d60e75c8a2dbb68982c57b4369d50d92117473
Tendai <youremail@example.com>
2023-09-23 13:58:14 +0200

    added new line declarATION

---
+++ increase2.asm
@@ -0,0 +1,5 @@
+.data
+    newline: .asciiz "\n"
+    inputFile: .asciiz "house_64_in_ascii_crlf.ppm"
+.text
+main:
--- increase_brightness.asm
+++ increase_brightness.asm
@@ -5,25 +5,25 @@
 .data
     originalPrompt: .asciiz "Average pixel value of the original image:\n"
     finalPrompt: .asciiz "Average pixel value of new image:\n"
-    input_file:  .asciiz "sample_images (1)/house_64_in_ascii_crlf.ppm"     #input file name
+    input_file:  .asciiz "house_64_in_ascii_crlf.ppm"     #input file name
     output_file:   .asciiz "house_64_in_ascii_crlf_output.ppm"    # New output file name
-    image_width:         .space 1024             # image_width for reading and writing 
+    image_width:    .space 1024             # image_width for reading and writing 
 
 .text
-    main:
-        # Open the original file for reading
-        li $v0, 13             #syscall 13 opens the file
-        la $a0, input_file #loadsthe address of the file
-        li $a1, 0              #reads e the file 
-        li $a2, 0             
-        syscall
+main:
+    # Open the original file for reading
+    li $v0, 13             #syscall 13 opens the file
+    la $a0, input_file #loadsthe address of the file
+    li $a1, 0              #reads e the file 
+    li $a2, 0             
+    syscall
+
+
+    bnez $v0, fileAccessed  # Checks if e file was opened 
 
     
-        bnez $v0, fileAccessed  # Checks if e file was opened 
-
-        
-        li $v0, 10    #exit If the file not  opened       
-        syscall  
+    li $v0, 10    #exit If the file not  opened       
+    syscall  
 
     fileAccessed:
         move $s0, $v0          #Saves file to $s0
@@ -99,90 +99,90 @@
 
         ble $t5, $t6, clamp_point      #   "   B at 255
         li $t5, 255
+
     clamp_point:
-        # Write the new RGB values
+        #write the new RGB values
         sb $t3, -3($t1)        #the new R component
         sb $t4, -2($t1)        #the new G component
         sb $t5, -1($t1)        #the new B component
 
-        # Increment the pixel counter
+        #increment the pixel counter
         addi $t2, $t2, 1
         b readPixels_loop
 
     create_image:
-        # Write the modified image to the new file
+        #write the modified image to the new file
         li $v0, 15             # syscall code for write
         move $a0, $s1         
         la $a1, image_width         # Source image_width
         li $a2, 512            
         syscall
 
-        # Close both files
+        #close both files
         li $v0, 16             # Load syscall code for close
         move $a0, $s0          # Original file descriptor
         syscall
         move $a0, $s1          
         syscall
 
-        # Calculate and display the average pixel values
+        #calculate and display the average pixel values
         li $t7, 0              #counter for  the original image
         li $t8, 0              # counter for  RGB values in the new image
         la $t1, image_width  
 
-        calculate_averages:
-            beq $t1, $t2, display_averages  # If the image_width pointer reaches the end, calculate and display averages
+    calculate_averages:
+        beq $t1, $t2, display_averages  # If the image_width pointer reaches the end, calculate and display averages
 
-            #calculate the sum of RGB values in the original image
-            lb $t9, -3($t1)        
-            lb $t6, -2($t1)       
-            lb $t11, -1($t1)       
+        #calculate the sum of RGB values in the original image
+        lb $t9, -3($t1)        
+        lb $t6, -2($t1)       
+        lb $t11, -1($t1)       
 
-            add $t7, $t7, $t9     # Add R component to the total for original image
-            add $t7, $t7, $t6    #G 
-            add $t7, $t7, $t11     #B 
+        add $t7, $t7, $t9     # Add R component to the total for original image
+        add $t7, $t7, $t6    #G 
+        add $t7, $t7, $t11     #B 
 
-            #calculate the sum of RGB values in the new image
-            lb $t12, -3($t1)       
-            lb $t13, -2($t1)       
-            lb $t15, -1($t1)       #load the modified B 
+        #calculate the sum of RGB values in the new image
+        lb $t12, -3($t1)       
+        lb $t13, -2($t1)       
+        lb $t15, -1($t1)       #load the modified B 
 
-            add $t8, $t8, $t12     
-            add $t8, $t8, $t13     
-            add $t8, $t8, $t15     
+        add $t8, $t8, $t12     
+        add $t8, $t8, $t13     
+        add $t8, $t8, $t15     
 
-            addi $t1, $t1, 3       #move image_width pointer to the next pixel
-            j calculate_averages
+        addi $t1, $t1, 3       #move image_width pointer to the next pixel
+        j calculate_averages
 
-        display_averages:
-            # Calculate the pixel
-            div $t2, $t7
-            mflo $f0               # store in $f0
+    display_averages:
+        # Calculate the pixel
+        div $t2, $t7
+        mflo $f0               # store in $f0
 
-            #prints out the original value prompt
-            li $v0, 4
-            la $a0, originalPrompt
-            syscall
+        #prints out the original value prompt
+        li $v0, 4
+        la $a0, originalPrompt
+        syscall
 
-            # Display the average pixel value of the original image
-            li $v0, 2              
-            syscall
+        # Display the average pixel value of the original image
+        li $v0, 2              
+        syscall
 
-            # Calculate the number of pixels
-            div $t2, $t8
-            mflo $f0               #store in $f0
+        # Calculate the number of pixels
+        div $t2, $t8
+        mflo $f0               #store in $f0
 
-            #prints out the final value prompt
-            li $v0, 4
-            la $a0, finalPrompt
-            syscall
+        #prints out the final value prompt
+        li $v0, 4
+        la $a0, finalPrompt
+        syscall
 
-            # Display the average pixel value of the new image
-            li $v0, 2              
-            syscall
+        # Display the average pixel value of the new image
+        li $v0, 2              
+        syscall
 
         #exit 
         li $v0, 10             
         syscall
 
 
-         
