ca5aa1bf39528399add43827af65874afab79cab
Tendai <youremail@example.com>
2023-09-25 22:46:08 +0200

    changed starting point of pointer

--- increase3.asm
+++ increase3.asm
@@ -41,49 +41,34 @@
         subu $t1, $t1, $t5 #subtract offset from length
 
     process_loop:
-        li $t2, 0
         lbu $t2, 0($t0) # load byte from buffer into $t2
-        subu $t2, $t2, 48 #convert ascii value to integer
-        addiu $t2, $t2, 10 # increase brightness by 10
-        sltiu $t3, $t2, 256 # check if value is less than 256
-        beqz $t3, clamp_value # if value is not less than 256, clamp it to 255
-        addiu $t2, $t2, 48 #covert back to ascii
-        sb $t2, 0($t0) # store byte back into buffer
-
+       
     digit_loop:
         subu $t2, $t2, 48 #ascii to integer
         mul $t2, $t2, 10 
-        addiu $t0, $t0, 1
         lbu $t3, 0($t0)
-        bne $t3, $t3, digit_loop
-
-        
-        lbu $t3, 0($t0)#load byte from buffer to t3
-        beqz $t3, done #if byte = null end of string exit loop
-        subu $t3, $t3, 48 #convert ascii digit to integer
-        mul $t2, $t2, 10 #shift current one place left
-        addiu $t0, $t0, 1 #increment bufer adress by 1
-        lbu $t3, 0($t0) #load next byte from buffer
-        bne $t3, 32, digit_loop 
+        li $s7, 32 #load ascii value for space into register s7
+        bne $t3, $s7, digit_loop
+        addiu $t0, $t0, 1
 
 
     done:
         addiu $t2, $t2, 10 #increase brightness by 10
-        sltiu $t3, $t3, 256 #check if less then 256
-        beqz $t3, clamp_value #if value is above 255
-        li $t2, 255+48 #cap to 255 and covert back to ascii
-        sb $t2, -1($t0) #store back into buffe
-        addiu $t1, $t1, -1 #decrement length by 1
-        bnez $t1, process_loop #if not zero continue loop
-
         
 
     clamp_value:
-        li $t2, 255+48 # clamp value to 255 and convert back to ascii
-        sb $t2, -1($t0) # store byte back into buffer
+        sltiu $t3, $t3, 256 #check if les than 255
+        beqz $t3, cap_value 
 
-        addiu $t0, $t0, -1 #decrement length by 4
-        bnez $t1, process_loop # if remaining length is not zero, continue loop
+    cap_value:
+        li $t2, 255+48 #cap at 255 and convert to ascii
+
+    store_value:
+        sb $t2, -1($t0) #store into buffer
+
+    next_pixel:
+        addiu $t1, $t1, -1 #decrement length by one
+        bnez $t1, process_loop #if not zero continue loop
 
         li $v0, 16
         move $a0, $s6
