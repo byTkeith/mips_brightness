a6544fcc738b0e35f3036719d46f373053b774f0
Tendai <youremail@example.com>
2023-09-27 20:13:25 +0200

    added a counter for pixels added

--- increase3.asm
+++ increase3.asm
@@ -2,10 +2,14 @@
     originalPrompt: .asciiz "Average pixel value of the original image:\n"
     finalPrompt: .asciiz "Average pixel value of new image:\n"
     
-    input_file:   .asciiz "D:\\arch_ass\\house_64_in_ascii_lf.ppm"    
-    output_file: .asciiz "D:\\arch_ass\\Result.ppm" 
-    buffer: .space 47434 # buffer to read data into
+    input_file:   .asciiz "D:/arch_ass/test.txt"    
+    output_file: .asciiz "D:/arch_ass/Result.ppm" 
+    buffer: .space 100 #47434 # buffer to read data into
+    buffer_output: .space 100 #47434 #buffer for upadated values
     offset: .word 4 #pixel offset
+    HEADER_SIZE: .space 18
+
+    newLine: .asciiz "\n"
 .globl main
 .text
     main:
@@ -13,100 +17,121 @@
         li $v0, 13 # system call for open file
         la $a0, input_file # input string
         li $a1, 0 # flag for read mode
-        li $a2, 0 # mode is ignored 
         syscall # open file
-
         move $s6, $v0 # save the file descriptor
 
         bltz $v0, exit #if our $v0 is less than 0
 
-        # Read from the file
-        li $v0, 14 # system call for read from file
-        move $a0, $s6 # file descriptor
-        la $a1, buffer # address of buffer to which to read
-        li $a2, 47434 # hardcoded buffer length
-        syscall # read from file
-
-        move $t4, $v0 #save the number of bytes
- 
-        bltz $v0, exit #checks if less than zero
-        
-        # Process the pixel data (increase brightness)
-        la $t0, buffer # load address of buffer into $t0
-        la $t5,  offset #skip the header and comment
-        lw $t5, 0($t5) #load word from offset into $t5
-
-        addu $t0, $t0, $t5 #add offset to buffer address
-        move $t1, $t4 #load length of buffer into t1
-        subu $t1, $t1, $t5 #subtract offset from length
-
-    process_loop:
-        lbu $t2, 0($t0) # load byte from buffer into $t2
-        subu $t2, $t2, 48
-        addiu $t2, $t2, 10
-
-    clamp_value: 
-        #clamp the value to 255
-        sltiu $t3, $t3, 256
-        beqz $t3, cap_value
-        
-
-    cap_value:
-        #convert to ascii then store
-        li $t3, 100
-        div $t2, $t3
-        mflo $t3
-        addiu $t3, $t3, 48
-        sb $t3, -3($t0) #store modified value at same position
-
-        li $t3, 10
-        rem $t6, $t2, $t3
-        mflo $t3
-
-        addiu $t3, $t3, 48
-        sb $t3, -2($t0) #stored at next position
-
-        mfhi $t2
-        addiu $t2, $t2, 48
-        sb $t2, -1($t0) #store at follow up position
-
-    next_pixel:
-        addiu $t1, $t1, -1 #decrement length by 3
-        bnez $t1, process_loop #if not zero 0r - continue loop
-
-        li $v0, 16
+        #read the header from input file
+        li $v0, 14
         move $a0, $s6
+        la $a1, buffer #adresss to read from
+        li $a2, 47434 #hard coded header size
         syscall
-
-        bltz $v0, exit
-
+	move $s2, $v0
+	
         #open output file
         li $v0, 13
         la $a0, output_file
-        li $a1, 1 #flag for write mode
-        li $a2, 0
-        syscall #open file
+        li $a1, 1 #flag fro write mode
+        syscall
+        move $s7, $v0 #save file discriptor
 
-        bltz $v0, exit
+        #write header to output_file
+        li $v0, 15
+        move $a0, $s7
+        la $a1, buffer
+        li $a2, 23
+        syscall
 
-        move $s7, $v0
+        #skip the header
+        la $t0, buffer
+#        la $t6, buffer_output
+        addiu $t0, $t0, 23 #skip header
+        
+        #compute the number of pixels
+        li $t8, '\n'
+        li $t7, '0' 
+        
+        addi $s2, $s2, -18
+    process_loop:
+        lbu $t2, 0($t0) # load byte from buffer into $t2
+        
+        #initialize pixel value to 0
+        li $t5, 0
+    convert_loop:
+    	beq $s2, $zero, process_pixel_value
+        beq $t2, $t8, process_pixel_value #if we have reached a space or newline character
+        sub $t2,$t2, $t7 #convert ascii to into
 
+        addi $s2, $s2, -1
+        mul $t5, $t5, 10
+        addu $t5, $t5,  $t2
+          
+        addiu $t0,$t0, 1
+        lbu $t2, ($t0) #load next character
+
+        j convert_loop
+
+    process_pixel_value:
+        move $t2, $t5 #move converted integr value to $t2
+        addiu $t2,$t2, 10 #increment pixel by 10
+      	addi $s2, $s2, -1
+      	
+  	bge $t2, 255, inc_set  #check if pixel less than 255
+    start_string:    
+	la $t6, buffer_output
+	addiu $t6, $t6, 11
+	
+	sb $zero, ($t6)
+	
+    store_value:
+        #convert back to string
+        convert_back_loop:
+            beqz $t2, end_convert_back
+            remu $t4, $t2, 10
+            addu $t4, $t4,'0'
+            sb $t4, -1($t6) #store in the correct order
+            addiu $t6,$t6, -1
+            divu $t2, $t2, 10
+            j convert_back_loop
+    end_convert_back:
+        #add line after each number
+ #       sb $t4, 0($t6)
+ #       addiu $t6, $t6, 1
+
+    max_value:
         #to write to file
         li $v0, 15
         move $a0, $s7 #output file discriptor
-        la $a1, buffer
-        move $a2, $t4
+        move $a1, $t6 #use bufferoutput instead
+        li $a2, 47434
         syscall
-
-        bltz $v0, exit
-
-        li $v0, 16
+        
+        move $s7, $zero
+        blez $s2, exit
+        #li $v0, 15
+        #move $a0, $s7 #output file discriptor
+        #la $a1, newLine
+        #li $a2, 1
+        #syscall
+	
+	#move $t6, $zero
+	#move $t5, $zero
+	
+	#blez $s2, exit
+	
+	j process_loop
+    exit:
+    	li $v0, 16
         move $a0, $s7
         syscall
-
-
-    exit:
+           
         li $v0, 10
         syscall
-
-
+        
+#li $v0, 10
+  #      syscall
+    inc_set:
+    	li $t2, 255		# set RGB value to 255, if over 255
+    	j start_string
---
+++ increase_brightness.asm
@@ -0,0 +1,258 @@
+#Tendai k Nyevedzanai
+#NYVTEN001
+#20/09/2023
+.data
+    originalAverage: .asciiz "Average pixel value of the original image:\n"
+    finalAverage: .asciiz "Average pixel value of new image:\n"
+    
+    input_file:   .asciiz "D:/arch_ass/test.txt"    
+    output_file: .asciiz "D:/arch_ass/Result.ppm" 
+    words_buffer: .asciiz 47434
+    org_buffer: .word 47434 # buffer to read data into
+    pixel_buffer: .space 47434 #buffer for upadated values
+    outcome: .space 64
+
+    float_size: .float 255.0
+
+    offset: .word 4 #pixel offset
+    HEADER_SIZE: .space 18
+
+    newLine: .asciiz "\n"
+.globl main
+.text
+    main:
+        # Open the file
+         move $t3, $zero #pointer for where to start
+
+        li $v0, 13 # system call for open file
+        la $a0, input_file # input string
+        li $a1, 0 # flag for read mode
+        syscall # open file
+        move $s0, $v0 # save the file descriptor
+
+        move $s5, $zero # initialising the pixel count
+        move $s6, $zero # lines containing pixels
+
+        move $s7, $zero #total value for pixels
+
+        #open output file
+        li $v0, 13
+        la $a0, output_file
+        li $a1, 1 #flag fro write mode
+        syscall
+
+        move $s3, $v0 # the number of bytes in the buffer
+
+        li $v0, 14
+        move $a0, $s0
+        la $a1, buffer #adresss to read from
+        li $a2, 47434 #hard coded header size
+        syscall
+
+        move $s2, $v0 #file descriptor
+
+        la $t0, words_buffer #address of buffer
+        li $t5, "\n" # new line
+        li $t7, '0'
+        li $t8, 10 
+
+        move $t4, $zero
+        
+    pixel_loop:
+        beq $t3, 4, process_loop #skipping the header to were the pixels start
+        lb $s1, 0($t0) #load byte of address
+
+        #write header to output_file
+        li $v0, 15
+        move $a0, $s3
+        la $a1, $t0
+        li $a2, 1 
+        syscall
+
+        addi $s2, $s2, -1 #decrementing the byte of the buffer
+        addi $t0, $t0, 1 # increment addresss
+        beq $s1, $t5, increase_by
+        j pixel_loop
+
+    process_loop:
+        #from string to integer
+        beq $s2, $zero, end_point #reached the end of file
+        lb $s1, 0($t0)
+        addi $t0, $t0, 1
+
+        beq $s1, $t5, endloop_2 #if values equal branch to the endloop_2
+        addi $s2, $s2, -1
+        sub $s1, $s1, $t7
+        mul $t4, $t4, 10
+        add $t4, $t4, $s1
+
+        j process_loop
+
+    increase_by:
+        addi $t3, $t3, 1
+        j pixel_loop
+
+    endloop_2:
+        addi $s2, $s2, -1 #decreasing size of the buffer
+        add $s7, $s7, $t4
+        addi $t4, $t4, 10 #incrementing values by 10
+        bge $t4, 255, increase_count #branch if value exceeds 255
+        add $s5, $s5, $t4 #adding total number of pixels
+        addi $s6, $s6,1
+
+    reverse_loop:
+        la $t1, pixel_buffer #pointer to output buffer
+        addiu $t1, $t1, 11 #pointer to last byte
+        sb $zero, ($t1) #null terminator
+
+        jal convert_loop
+        move $t6, $zero #moving length of string
+        move $s4, $t1 #move address of string to $s4
+        jal maxLength
+
+    copy_to_output:
+        #write  to output_file
+        li $v0, 15
+        move $a0, $s3 #file descriptor
+        move $a1, $t1 #values written to output
+        move $a2, $t6 #length of string
+        syscall
+
+        #write new pixel to output_file
+        li $v0, 15
+        move $a0, $s3
+        la $a1, newLine
+        li $a2, 1
+        syscall
+
+        move $t1, $zero
+        move $t4, $zero
+        blez $s2, final_loop
+
+        j process_loop
+
+    end_point:
+        j endloop_2
+
+    final_loop:
+        #convert int to floating pointer
+        lwc1 $f6, float_size
+        mtc1 $s7, $f4 #move $s7 to floating point reg f4, for old values
+        mtc1 $s5, $f0 #move the new values to $f0
+        mtc1 $s6, $f2 #move s6 to fp reg f2
+
+        div.s $f4 , $f4, $f2 #f4/f2=f4 for old image
+        div.s $f4 $f4, $f6 #f4/255=f4
+
+        div.s $f0, $f0, $f2 #f0/f2=f0
+        div.s $f0, $f0, $f6
+
+        #print out the old average prompt 
+        li $v0, 4
+        la $a0, originalAverage
+        syscall
+
+        li $vo, 2
+        mov.s $f12, $f4 #to print the float of old values
+        syscall
+
+        #print out the new average prompt 
+        li $v0, 4
+        la $a0, finalAverage
+        syscall
+
+        li $vo, 2
+        mov.s $f12, $f0 #to print the float of old values
+        syscall
+
+        li $v0, 16
+        move $a0, $s0
+        syscall
+
+        li $v0, 16
+        move $a0, $s3
+        syscall
+
+        li $v0, 10 #exit
+        syscall
+    out_of_bounds:
+        negu $t4, $t4
+        sb $t4, -1($t1)
+        j convert_loop
+
+    convert_loop:
+        #get next digit
+        remu $t2, $t4, 10
+        addiu $t2, $t2, '0' #conert to ascii
+        addiu $t1, $t1, -1 #move the pointer
+        sb $t2, 0($t1)  #store value in the buffer
+        divu $t4, $t4, 10 #
+        bnez $t4, convert_loop
+
+        jr $ra
+
+    maxLength:
+        lb $t2, 0($s4)
+        beq $t2, $zero, , copy_to_output
+        addi $t6, $t6, 1
+        addi $s4, $s4, 1
+        j maxLength
+
+    increase_count:
+        li $t4, 255 #clamp values to 255
+        j reverse_loop 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    #inc_sets
+        li $t4, 255
+        j out_of_bounds #end strt
+
+
+
+        
+
+
+
+
+
+
